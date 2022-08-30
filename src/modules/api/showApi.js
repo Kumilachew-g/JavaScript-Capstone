e60b06f81297116d941da4a93f39c9ae374a0660
Tadesse Jemal <tadessejemal63@gmail.com>
2022-08-30 22:21:28 +0300

    create show api

---
+++ src/modules/api/showApi.js
@@ -0,0 +1,32 @@
+const mainUrl = 'https://api.tvmaze.com/shows';
+const involvementMainUrl =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
+const apiId = 'DDM3BnYbiCcIIPt2Rdqx';
+const lookupFetch = async (showId) => {
+  const url = `${mainUrl}/shows/${showId}`;
+  let show = await fetch(url);
+  show = await show.json();
+  return show;
+};
+const fetchComments = async (showId) => {
+  const url = `${involvementMainUrl}/${apiId}/comments?item_id=${showId}`;
+  let comments = await fetch(url);
+  comments = await comments.json();
+  return comments;
+};
+const showCommentsInApi = async (showId, name, insight) => {
+  const url = `${involvementMainUrl}/${apiId}/comments`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: `${showId}`,
+      username: name,
+      comment: insight,
+    }),
+  });
+  return response.json();
+};
+export { lookupFetch, fetchComments, showCommentsInApi };
