f8dee398068a54446044ff2c887b5f06e12618fc
Tadesse Jemal <tadessejemal63@gmail.com>
2022-08-31 23:31:00 +0300

    display-likes and new likes

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const moviesShows = [];
 
 document.addEventListener('DOMContentLoaded', async () => {
   moviesShows.push(
+    await lookupFetch(90),
+    await lookupFetch(91),
+    await lookupFetch(92),
+    await lookupFetch(93),
+    await lookupFetch(94),
+    await lookupFetch(95),
+    await lookupFetch(96),
+    await lookupFetch(1),
+    await lookupFetch(98),
     await lookupFetch(99),
-    await lookupFetch(101),
-    await lookupFetch(102),
-    await lookupFetch(103),
-    await lookupFetch(104),
-    await lookupFetch(105),
-    await lookupFetch(106),
-    await lookupFetch(112),
-    await lookupFetch(108),
-    await lookupFetch(109),
-    await lookupFetch(110),
+    await lookupFetch(11),
     await lookupFetch(111),
   );
   await createMoviesApp();
